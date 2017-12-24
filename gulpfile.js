45378072b14757025067ad2adfcde304b73a6367
Dmitry Anufriev <dmitryanufriev@gmail.com>
2017-12-24 07:30:26 +0500

    Setup styles build. Add less support.

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,52 @@
-var gulp = require('gulp');
-var gulp_tslint = require('gulp-tslint');
-var ts = require('gulp-typescript');
+var gulp = require("gulp");
+var gulp_tslint = require("gulp-tslint");
+var ts = require("gulp-typescript");
+var less = require("gulp-less");
+var minifyCSS = require("gulp-minify-css");
+var rename = require("gulp-rename");
+var LessAutoprefix = require("less-plugin-autoprefix");
 
+var autoprefix = new LessAutoprefix({ browsers: ["last 2 versions"] });
 
 /* Backend */
 
-var tsSrc = './src/backend/**/*.ts';
+var tsSrc = "./src/backend/**/*.ts";
 
-gulp.task('backend:lint:ts', function () {
-    return gulp.src(tsSrc)
+gulp.task("backend:lint:ts", function() {
+    return gulp
+        .src(tsSrc)
         .pipe(gulp_tslint())
         .pipe(gulp_tslint.report());
 });
 
-gulp.task('backend:compile:ts', function () {
-    var tsProject = ts.createProject('tsconfig.json');
-    var tsResult = gulp.src(tsSrc)
-        .pipe(tsProject());
-    return tsResult.js.pipe(
-        gulp.dest('./dist')
-    );
+gulp.task("backend:compile:ts", function() {
+    var tsProject = ts.createProject("tsconfig.json");
+    var tsResult = gulp.src(tsSrc).pipe(tsProject());
+    return tsResult.js.pipe(gulp.dest("./dist"));
 });
 
-gulp.task("backend:views:copy", function () {
-    gulp.src("./src/backend/views/**/*")
-        .pipe(gulp.dest("./dist/views"));
+gulp.task("backend:views:copy", function() {
+    gulp.src("./src/backend/views/**/*").pipe(gulp.dest("./dist/views"));
 });
 
-gulp.task("default", ["backend:lint:ts", "backend:compile:ts", "backend:views:copy"]);
+/* Frontend */
+
+gulp.task("frontend:compile:less", function() {
+    gulp
+        .src("./src/frontend/styles/styles.less")
+        .pipe(
+            less({
+                plugins: [autoprefix]
+            })
+        )
+        .pipe(minifyCSS())
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(gulp.dest("./dist/public/styles"));
+});
+
+gulp.task("default", [
+    "backend:lint:ts",
+    "backend:compile:ts",
+    "backend:views:copy",
+    "frontend:compile:less"
+]);
