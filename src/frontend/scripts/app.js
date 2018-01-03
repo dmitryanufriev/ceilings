6f14ee30104a6709af8ee48db1c5b6f528775b91
Dmitry Anufriev <dmitryanufriev@gmail.com>
2018-01-03 10:59:54 +0500

    Notify about error.

--- src/frontend/scripts/app.js
+++ src/frontend/scripts/app.js
@@ -4,26 +4,31 @@ var Inputmask = require("inputmask");
 
 var scrollToPageMixin = {
     methods: {
-        scrollToPage: function (event) {
+        scrollToPage: function(event) {
             event.preventDefault();
             var el = event.target;
             var pageId = $(el).attr("href");
             // Figure out element to scroll to
             var page = $(pageId);
-            $("html, body").animate({
-                scrollTop: page.offset().top
-            }, 500, function () {
-                // Callback after animation
-                // Must change focus!
-                var $target = $(page);
-                $target.focus();
-                if ($target.is(":focus")) { // Checking if the target was focused
-                    return false;
-                } else {
-                    $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                    $target.focus(); // Set focus again
+            $("html, body").animate(
+                {
+                    scrollTop: page.offset().top
+                },
+                500,
+                function() {
+                    // Callback after animation
+                    // Must change focus!
+                    var $target = $(page);
+                    $target.focus();
+                    if ($target.is(":focus")) {
+                        // Checking if the target was focused
+                        return false;
+                    } else {
+                        $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                        $target.focus(); // Set focus again
+                    }
                 }
-            });
+            );
         }
     }
 };
@@ -34,8 +39,8 @@ var homePage = new Vue({
     el: "#home",
     mixins: [scrollToPageMixin],
     methods: {
-        backCall: function (modalId) {
-            bus.$emit("back-call:show")
+        backCall: function(modalId) {
+            bus.$emit("back-call:show");
         }
     }
 });
@@ -43,20 +48,20 @@ var homePage = new Vue({
 var portfolioPage = new Vue({
     el: "#portfolio",
     mixins: [scrollToPageMixin],
-    data: function () {
+    data: function() {
         return {
             images: []
         };
     },
-    created: function () {
+    created: function() {
         var self = this;
         axios.get("/portfolio").then(
-            function (response) {
+            function(response) {
                 for (var i = 0; i < response.data.images.length; i++) {
                     self.images.push(response.data.images[i]);
                 }
             },
-            function (response) {
+            function(response) {
                 // error callback
                 // console.error(response.body);
             }
@@ -66,18 +71,16 @@ var portfolioPage = new Vue({
 
 var backCallModal = new Vue({
     el: "#modalBackCall",
-    created: function () {
+    created: function() {
         var self = this;
-        bus.$on("back-call:show", function () {
+        bus.$on("back-call:show", function() {
             self.toggle.call(self);
         });
     },
-    mounted: function () {
-        Inputmask().mask(
-            this.$el.getElementsByTagName("input")
-        );
+    mounted: function() {
+        Inputmask().mask(this.$el.getElementsByTagName("input"));
     },
-    data: function () {
+    data: function() {
         return {
             shown: false,
             form: {
@@ -90,70 +93,79 @@ var backCallModal = new Vue({
                     phone: false
                 }
             }
-        }
+        };
     },
     computed: {
-        phone: function () {
+        phone: function() {
             return this.form.fields.phone
-                ? this.form.fields.phone
-                    .match(/\d+/g)
-                    .join("")
+                ? this.form.fields.phone.match(/\d+/g).join("")
                 : "";
         },
-        name: function () {
+        name: function() {
             return this.form.fields.name;
         },
-        time: function () {
+        time: function() {
             return this.form.fields.time;
         },
-        hasErrors: function () {
+        hasErrors: function() {
             return this.form.errors.phone;
         }
     },
     methods: {
-        toggle: function () {
+        toggle: function() {
             this.form.errors.phone = false;
             $(this.$el).modal("toggle");
         },
-        validate: function () {
+        validate: function() {
             this.validatePhone();
         },
-        validatePhone: function () {
+        validatePhone: function() {
             this.form.errors.phone = this.phone.length !== 10;
         },
-        send: function (event) {
+        send: function(event) {
             event.preventDefault();
             event.stopPropagation();
             this.validate();
-            if (this.hasErrors)
-                return;
+            if (this.hasErrors) return;
             this.toggle();
-            axios.post("/backcall", {
-                phone: this.phone,
-                name: this.name,
-                time: this.time
-            }).then(function () {
-                bus.$emit("notify:success", {
-                    title: "Спасибо!",
-                    text: "Заявка на обратный звонок отправлена. Наш администратор свяжется с Вами в указанное или ближайшее время."
+            axios
+                .post("/backcall", {
+                    phone: this.phone,
+                    name: this.name,
+                    time: this.time
+                })
+                .then(function(response) {
+                    bus.$emit("notify:success", {
+                        title: "Спасибо!",
+                        text:
+                            "Заявка на обратный звонок отправлена. Наш администратор свяжется с Вами в указанное или ближайшее время."
+                    });
+                })
+                .catch(function(error) {
+                    bus.$emit("notify:error", {
+                        title: "Ошибка отправки",
+                        text:
+                            "К сожалению, не удалось отправить заявку на обратный звонок. Попробуйте повторить позже."
+                    });
                 });
-            }).catch(function (error) {
-                console.log("Fail...");
-            });
         }
     }
 });
 
 var notification = new Vue({
     el: "#notification",
-    created: function () {
+    created: function() {
         var self = this;
-        bus.$on("notify:success", function (notification) {
+        bus.$on("notify:success", function(notification) {
             self.success.call(self, notification);
         });
+        bus.$on("notify:error", function(notification) {
+            self.error.call(self, notification);
+        });
     },
-    data: function () {
+    data: function() {
         return {
+            type: "success",
             timer: undefined,
             visible: false,
             title: "",
@@ -161,22 +173,25 @@ var notification = new Vue({
         };
     },
     methods: {
-        open: function () {
+        open: function(type, notification) {
             var self = this;
+            self.type = type;
+            self.title = notification.title || "";
+            self.text = notification.text || "";
             self.visible = true;
-            self.timer = setTimeout(function () {
+            self.timer = setTimeout(function() {
                 self.visible = false;
             }, 5000);
         },
-        close: function () {
+        close: function() {
             clearTimeout(this.timer);
             this.visible = false;
         },
-        success: function (notification) {
-            var self = this;
-            self.title = notification.title || "";
-            self.text = notification.text || "";
-            self.open();
+        success: function(notification) {
+            this.open("success", notification);
+        },
+        error: function(notification) {
+            this.open("error", notification);
         }
     }
 });
