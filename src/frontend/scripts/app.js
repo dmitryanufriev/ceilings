3a2a71df9daec70217e3b77ad0c1d9212a362947
Dmitry Anufriev <dmitryanufriev@gmail.com>
2018-01-05 11:03:28 +0500

    Place share button below portfolio images carousel. Add handler for click on share on Facebook.

--- src/frontend/scripts/app.js
+++ src/frontend/scripts/app.js
@@ -4,7 +4,7 @@ var Inputmask = require("inputmask");
 
 var scrollToPageMixin = {
     methods: {
-        scrollToPage: function(event) {
+        scrollToPage: function (event) {
             event.preventDefault();
             var el = event.target;
             var pageId = $(el).attr("href");
@@ -15,7 +15,7 @@ var scrollToPageMixin = {
                     scrollTop: page.offset().top
                 },
                 500,
-                function() {
+                function () {
                     // Callback after animation
                     // Must change focus!
                     var $target = $(page);
@@ -39,7 +39,7 @@ var homePage = new Vue({
     el: "#home",
     mixins: [scrollToPageMixin],
     methods: {
-        backCall: function(modalId) {
+        backCall: function (modalId) {
             bus.$emit("back-call:show");
         }
     }
@@ -47,21 +47,41 @@ var homePage = new Vue({
 
 var portfolioPage = new Vue({
     el: "#portfolio",
-    mixins: [scrollToPageMixin]
+    mixins: [scrollToPageMixin],
+    methods: {
+        shareOnFacebook: function () {
+            console.log("Share on Facebook");
+            console.log(window.location.href);
+            // var width = 600;
+            // var height = 400;
+            // var windowLeft = window.screenLeft ? window.screenLeft : window.screenX;
+            // var windowTop = window.screenTop ? window.screenTop : window.screenY;
+            // var left = windowLeft + (window.innerWidth / 2) - (width / 2);
+            // var top = windowTop + (window.innerHeight / 2) - (height / 2);
+            // var facebookWindow = window.open(
+            //     "https://www.facebook.com/sharer/sharer.php?u=https://www.instagram.com/p/BdWnAY1Fl0-/",
+            //     "Share on Facebook",
+            //     "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" + width + ', height=' + height + ', top=' + top + ', left=' + left
+            // );
+            // if (window.focus) {
+            //     facebookWindow.focus();
+            // }
+        }
+    }
 });
 
 var backCallModal = new Vue({
     el: "#modalBackCall",
-    created: function() {
+    created: function () {
         var self = this;
-        bus.$on("back-call:show", function() {
+        bus.$on("back-call:show", function () {
             self.toggle.call(self);
         });
     },
-    mounted: function() {
+    mounted: function () {
         Inputmask().mask(this.$el.getElementsByTagName("input"));
     },
-    data: function() {
+    data: function () {
         return {
             shown: false,
             form: {
@@ -77,33 +97,33 @@ var backCallModal = new Vue({
         };
     },
     computed: {
-        phone: function() {
+        phone: function () {
             return this.form.fields.phone
                 ? this.form.fields.phone.match(/\d+/g).join("")
                 : "";
         },
-        name: function() {
+        name: function () {
             return this.form.fields.name;
         },
-        time: function() {
+        time: function () {
             return this.form.fields.time;
         },
-        hasErrors: function() {
+        hasErrors: function () {
             return this.form.errors.phone;
         }
     },
     methods: {
-        toggle: function() {
+        toggle: function () {
             this.form.errors.phone = false;
             $(this.$el).modal("toggle");
         },
-        validate: function() {
+        validate: function () {
             this.validatePhone();
         },
-        validatePhone: function() {
+        validatePhone: function () {
             this.form.errors.phone = this.phone.length !== 10;
         },
-        send: function(event) {
+        send: function (event) {
             event.preventDefault();
             event.stopPropagation();
             this.validate();
@@ -115,14 +135,14 @@ var backCallModal = new Vue({
                     name: this.name,
                     time: this.time
                 })
-                .then(function(response) {
+                .then(function (response) {
                     bus.$emit("notify:success", {
                         title: "Спасибо!",
                         text:
                             "Заявка на обратный звонок отправлена. Наш администратор свяжется с Вами в указанное или ближайшее время."
                     });
                 })
-                .catch(function(error) {
+                .catch(function (error) {
                     bus.$emit("notify:error", {
                         title: "Ошибка отправки",
                         text:
@@ -135,16 +155,16 @@ var backCallModal = new Vue({
 
 var notification = new Vue({
     el: "#notification",
-    created: function() {
+    created: function () {
         var self = this;
-        bus.$on("notify:success", function(notification) {
+        bus.$on("notify:success", function (notification) {
             self.success.call(self, notification);
         });
-        bus.$on("notify:error", function(notification) {
+        bus.$on("notify:error", function (notification) {
             self.error.call(self, notification);
         });
     },
-    data: function() {
+    data: function () {
         return {
             type: "success",
             timer: undefined,
@@ -154,24 +174,24 @@ var notification = new Vue({
         };
     },
     methods: {
-        open: function(type, notification) {
+        open: function (type, notification) {
             var self = this;
             self.type = type;
             self.title = notification.title || "";
             self.text = notification.text || "";
             self.visible = true;
-            self.timer = setTimeout(function() {
+            self.timer = setTimeout(function () {
                 self.close.call(self);
             }, 5000);
         },
-        close: function() {
+        close: function () {
             clearTimeout(this.timer);
             this.visible = false;
         },
-        success: function(notification) {
+        success: function (notification) {
             this.open("success", notification);
         },
-        error: function(notification) {
+        error: function (notification) {
             this.open("error", notification);
         }
     }
