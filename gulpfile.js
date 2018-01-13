444e202d8a3891cba8d4ae53f3c6a75b56301884
Dmitry Anufriev <dmitryanufriev@gmail.com>
2018-01-13 16:45:09 +0500

    Compile Vue in production mode.

--- gulpfile.js
+++ gulpfile.js
@@ -5,38 +5,39 @@ var less = require("gulp-less");
 var minifyCSS = require("gulp-minify-css");
 var rename = require("gulp-rename");
 var browserify = require("browserify");
+var envify = require("envify/custom");
 var source = require("vinyl-source-stream");
 var uglify = require("gulp-uglify");
 var buffer = require("vinyl-buffer");
 var LessAutoprefix = require("less-plugin-autoprefix");
 
-var autoprefix = new LessAutoprefix({ browsers: ["last 2 versions"] });
+var autoprefix = new LessAutoprefix({browsers: ["last 2 versions"]});
 
 /* Backend */
 
 var tsSrc = "./src/backend/**/*.ts";
 
-gulp.task("backend:lint:ts", function() {
+gulp.task("backend:lint:ts", function () {
     return gulp
         .src(tsSrc)
         .pipe(gulp_tslint())
         .pipe(gulp_tslint.report());
 });
 
-gulp.task("backend:compile:ts", function() {
+gulp.task("backend:compile:ts", function () {
     var tsProject = ts.createProject("tsconfig.json");
     var tsResult = gulp.src(tsSrc).pipe(tsProject());
     return tsResult.js.pipe(gulp.dest("./dist"));
 });
 
-gulp.task("backend:copy:views", function() {
+gulp.task("backend:copy:views", function () {
     gulp.src("./src/backend/views/**/*").pipe(gulp.dest("./dist/views"));
 });
 
 
 /* Frontend */
 
-gulp.task("frontend:compile:less", function() {
+gulp.task("frontend:compile:less", function () {
     gulp
         .src("./src/frontend/styles/styles.less")
         .pipe(
@@ -45,31 +46,38 @@ gulp.task("frontend:compile:less", function() {
             })
         )
         .pipe(minifyCSS())
-        .pipe(rename({ suffix: ".min" }))
+        .pipe(rename({suffix: ".min"}))
         .pipe(gulp.dest("./dist/public/styles"));
 });
 
-gulp.task("frontend:font:copy", function() {
+gulp.task("frontend:font:copy", function () {
     gulp.src("./src/frontend/font/*").pipe(gulp.dest("./dist/public/font"));
 });
 
-gulp.task("frontend:font:styles:copy", function() {
+gulp.task("frontend:font:styles:copy", function () {
     gulp
         .src("./src/frontend/styles/font/*")
         .pipe(gulp.dest("./dist/public/styles"));
 });
 
-gulp.task("frontend:images:copy", function() {
+gulp.task("frontend:images:copy", function () {
     gulp
         .src("./src/frontend/images/**/*")
         .pipe(gulp.dest("./dist/public/images"));
 });
 
-gulp.task("frontend:compile:js", function() {
+gulp.task("frontend:compile:js", function () {
     browserify({
         entries: "./src/frontend/scripts/app.js",
         debug: false
     })
+        .transform({
+                global: true
+            },
+            envify({
+                NODE_ENV: 'production'
+            })
+        )
         .bundle()
         .pipe(source("scripts.min.js"))
         .pipe(buffer())
